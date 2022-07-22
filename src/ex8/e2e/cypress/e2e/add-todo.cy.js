c36c76a47f41d7d050d427d45544bad5407559d5
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-07-22 13:36:46 +0300

    fix:remove ex8 folder

--- src/ex8/e2e/cypress/e2e/add-todo.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Add Todo Action", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
-
-  it("Should add a new todo", () => {
-    // TODO: fill this test
-  });
-});
